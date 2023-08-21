28dae9082b2658080768bf7fad135a575d2214b9
ibvwvq <ikramova_arina@mail.ru>
2023-08-21 20:39:10 +0300

    правки в названиях переменных

--- routes/console.php
+++ routes/console.php
@@ -27,7 +27,7 @@ Artisan::command('fourth_task_mono', function () {
                 $ratings = DB::table('ratings')->where('user_id', $user->id)->get(); // все рейтинги одного пользователя
                 $products = DB::table('products')->where('priceProduct', '>', 3000)->get(); // продукты цена которых больше 3000
                 $countRating = 0; // переменная для среднего рейтинга
-                $count = 0; // переменная для счета кол-ва нужных рейтингов
+                $count = 0; // переменная для счета кол-ва  рейтингов на продукты с ценником больше 3000
 
                 // просчитываем средний рейтинг
                 foreach ($ratings as $rating) {
@@ -37,31 +37,31 @@ Artisan::command('fourth_task_mono', function () {
                 // первый запрос - Получить все отзывы, оставленные пользователями из Самары или Волгограда, отзывы которых были полезны для более чем 10 пользователей,
                 if ($user->cityUser == 'Volgograd' || $user->cityUser == 'Samara') {
                     foreach ($ratings as $rating) {
-                        $rat = Rating::find($rating->id); // ищем конкретный рейтинг
-                        $feed = $rat->feedback; // находим отзыв у рейтинга (один рейтинг - один отзыв)
+                        $currentRating = Rating::find($rating->id); // ищем конкретный рейтинг
+                        $currentFeedback = $currentRating->feedback; // находим отзыв у рейтинга (один рейтинг - один отзыв)
 
                         // проверяем на кол-во отметок "отзыв полезен" - если больше 10, то добавляем в массив
-                        if ($feed->countMarkFeedback > 10) {
-                            $array[] = $feed->id;
+                        if ($currentFeedback->countMarkFeedback > 10) {
+                            $array[] = $currentFeedback->id;
                         }
                     }
-                } // второй запрос - которые оставили более 10 отзывов на товары стоимостью более 3 т.р., при среднем рейтинге всех отзывов пользователя на такие товары более 4.
+                } // второй запрос - Получить все отзывы, оставленные пользователями которые оставили более 10 отзывов на товары стоимостью более 3 т.р., при среднем рейтинге всех отзывов пользователя на такие товары более 4.
                 else{
                     foreach ($ratings as $rating) {
                         // перебираем продукты
                         foreach ($products as $product) {
-                            // просчитываем сколько пользователь оставлял рейтингов на продукты с ценником больше 3000
+                            // просчитываем сколько пользователь оставлял рейтингов на продукты с ценником больше 3000 (на 1 рейтинг есть 1 отзыв)
                             if ($rating->product_id == $product->id) {
                                 $count++;
                             }
                         }
 
-                        $rat = Rating::find($rating->id);
-                        $feed = $rat->feedback; // находим отзыв у рейтинга (один рейтинг - один отзыв)
+                        $currentRating = Rating::find($rating->id);
+                        $currentFeedback = $currentRating->feedback; // находим отзыв у рейтинга (один рейтинг - один отзыв)
 
                         // если пользователь оставил больше чем 10 рейтингов на товары стоимостью больше 300 и если средний рейтинг больше 4
                         if ($count > 10 && $countRating / ($ratings->count()) > 4) {
-                            $array[] = $feed->id;
+                            $array[] = $currentFeedback->id;
                         }
                     }
                 }
